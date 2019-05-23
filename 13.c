092479ccbe31831c51b5cd8a1cc58d8b83d8070b
IamVISHNURAJ <50854010+IamVISHNURAJ@users.noreply.github.com>
2019-05-23 16:02:59 +0530

    Add files via upload

---
+++ 13.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+int n, c = 2;
+scanf("%d",&n);
+for ( c = 2 ; c <= n - 1 ; c++ )
+{
+if ( n%c == 0 )
+{
+printf("no",);
+break;
+}
+}
+if ( c == n )
+printf("yes",);
+return 0;
+}
