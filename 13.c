a369f8825785ff5cd09e7351b558eab2e5976710
IamVISHNURAJ <50854010+IamVISHNURAJ@users.noreply.github.com>
2019-05-23 16:04:04 +0530

    Update 13.c

--- 13.c
+++ 13.c
@@ -3,15 +3,15 @@ main()
 {
 int n, c = 2;
 scanf("%d",&n);
-for ( c = 2 ; c <= n - 1 ; c++ )
+for (c=2;c<=n-1;c++)
 {
-if ( n%c == 0 )
+if (n%c==0)
 {
 printf("no",);
 break;
 }
 }
-if ( c == n )
+if (c==n)
 printf("yes",);
 return 0;
 }
